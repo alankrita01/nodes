0edaecd821dd01c5b507960963a78eb392f6ce93
Alankrita Patidar <2102561>
2022-08-02 22:55:57 +0530

    queryselector added

--- item.js
+++ item.js
@@ -18,7 +18,7 @@ for(var i=0; i<items.length; i++){
 */
 
 // get element by tag name
-var li=document.getElementsByTagName('li');
+/*var li=document.getElementsByTagName('li');
 console.log(li);
 console.log(li[1]);
 
@@ -28,12 +28,57 @@ li[1].style.background = '#f4f4f4';
 
 for(var i=0; i<li.length; i++){
 li[i].style.fontWeight='bold';
-}
+}*/
 
-li[4].style.color='red';
+//li[4].style.color='red';
 // QuerySelector
 
 var header = document.querySelector('#main-header');
 header.style.borderBottom = 'solid 4px #ccc';
 
+var input = document.querySelector('input');
+input.value = 'Hello World';
 
+
+var submit = document.querySelector('input[type="submit"]');
+submit.value='SEND';
+
+var item= document.querySelector('.list-group-item');
+item.style.color='red';
+
+var lastItem = document.querySelector
+('.list-group-item:last-child');
+lastItem.style.color='blue';
+
+
+
+
+
+//Querey selector all
+
+var titles = document.querySelectorAll('.title');
+console.log(titles);
+
+titles[0].textContent = 'Hello';
+
+
+
+var secondItem = document.querySelector
+('.list-group-item:nth-child(2)');
+secondItem.style.background='green';
+
+var thirdItem = document.querySelector
+('.list-group-item:nth-child(3)');
+thirdItem.style.visibility = "hidden";
+
+var second=document.querySelectorAll(".list-group-item");
+second[1].style.color='green';
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+var even = document.querySelectorAll('li:nth-child(even)');
+
+for(var i=0;i<odd.length;i++){
+  odd[i].style.background='green';
+  even[i].style.background='#ccc';
+
+}
